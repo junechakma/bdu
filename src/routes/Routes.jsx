da39f92fdad8fad2628f4e944e6b27a00ac332d4
junechakma <123475846+blnderx@users.noreply.github.com>
2024-01-07 00:01:24 +0600

    First Page

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,7 +1,6 @@
 import { createBrowserRouter } from 'react-router-dom'
 import Index from '../layout/Index'
 import Home from '../pages/Home/Home'
-import About from '../pages/about/about'
 
 export const router = createBrowserRouter([
     {
