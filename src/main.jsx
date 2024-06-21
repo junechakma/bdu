3df28e912a640ae9dcbb1a97056259f8ad44ee64
junechakma <junechakma50@gmail.com>
2024-06-22 00:05:28 +0600

    Scroll Issue

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,12 @@ import './index.css'
 
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes/Routes'
+import ScrollToTop from './components/ScrollToTop'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <ScrollToTop>
     <RouterProvider router={router} />
+    </ScrollToTop>
   </React.StrictMode>
 )
